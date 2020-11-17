ec7128eaa6414a83461bb3dd10d094cc36afb0d2
SimonIT <simonit.orig@gmail.com>
2020-11-17 22:35:58 +0100

    Fix dump mistake

--- pyinstaller_hooks/hook-sentry_sdk.py
+++ pyinstaller_hooks/hook-sentry_sdk.py
@@ -1,10 +1,5 @@
 import sentry_sdk.integrations as si
 
-
-def make_integration_name(integration_name: str):
-    return ".".join(integration_name.split(".")[:-1])
-
-
 hiddenimports = ["sentry_sdk.integrations.stdlib",
                  "sentry_sdk.integrations.excepthook",
                  "sentry_sdk.integrations.dedupe",
@@ -12,4 +7,11 @@ hiddenimports = ["sentry_sdk.integrations.stdlib",
                  "sentry_sdk.integrations.modules",
                  "sentry_sdk.integrations.argv",
                  "sentry_sdk.integrations.logging",
-                 "sentry_sdk.integrations.threading"].extend(map(make_integration_name, si._AUTO_ENABLING_INTEGRATIONS))
+                 "sentry_sdk.integrations.threading"]
+
+
+def make_integration_name(integration_name: str):
+    return ".".join(integration_name.split(".")[:-1])
+
+
+hiddenimports.extend(list(map(make_integration_name, si._AUTO_ENABLING_INTEGRATIONS)))
