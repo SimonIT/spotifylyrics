838a67e5cd64f078db4f4ec0eac5aae540e3f28a
SimonIT <simonit.orig@gmail.com>
2020-11-17 22:13:56 +0100

    Get dynamically all auto enbaled integrations and add them as hidden imports

--- pyinstaller_hooks/hook-sentry_sdk.py
+++ pyinstaller_hooks/hook-sentry_sdk.py
@@ -1,1 +1,15 @@
-hiddenimports = ["sentry_sdk.integrations.*"]
+import sentry_sdk.integrations as si
+
+
+def make_integration_name(integration_name: str):
+    return ".".join(integration_name.split(".")[:-1])
+
+
+hiddenimports = ["sentry_sdk.integrations.stdlib",
+                 "sentry_sdk.integrations.excepthook",
+                 "sentry_sdk.integrations.dedupe",
+                 "sentry_sdk.integrations.atexit",
+                 "sentry_sdk.integrations.modules",
+                 "sentry_sdk.integrations.argv",
+                 "sentry_sdk.integrations.logging",
+                 "sentry_sdk.integrations.threading"].extend(map(make_integration_name, si._AUTO_ENABLING_INTEGRATIONS))
