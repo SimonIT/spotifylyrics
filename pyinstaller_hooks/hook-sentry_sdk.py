17f91f9f3ebff4f7110c88648ce842781a3aec73
SimonIT <simonit.orig@gmail.com>
2020-11-17 21:05:40 +0100

    Add sentry flask hook

--- pyinstaller_hooks/hook-sentry_sdk.py
+++ pyinstaller_hooks/hook-sentry_sdk.py
@@ -6,4 +6,5 @@ hiddenimports = ["sentry_sdk.integrations.stdlib",
                  "sentry_sdk.integrations.argv",
                  "sentry_sdk.integrations.logging",
                  "sentry_sdk.integrations.threading",
-                 "sentry_sdk.integrations.django"]
+                 "sentry_sdk.integrations.django",
+                 "sentry_sdk.integrations.flask"]
